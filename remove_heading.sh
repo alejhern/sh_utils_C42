66391eb75b90650c5a5c89bf65cec19133e44105
Alejandro Hernández Hernández <alejandro1998hernandez@gmail.com>
2024-06-13 05:46:48 +0200

    se han añadido validaciones

--- remove_heading.sh
+++ remove_heading.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-tail -n +13 "$1" | tee "$1" > /dev/null
+# Comprobar si se proporcionaron los argumentos necesarios
+if [ "$#" -ne 1 ]; then
+    echo "Uso: $0 <ruta_archivo_c>"
+    exit 1
+fi
+
+FILE="$1"
+
+#Comprobamos si el FILE existe
+if [ ! -f "$FILE" ]; then
+    echo "El archivo $FILE no existe."
+    exit 1
+fi
+
+tail -n +13 "$FILE" | tee "$FILE" > /dev/null
